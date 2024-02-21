b122b0b6d994411a1c590e3243d7579d98c73fdb
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2024-02-21 17:52:03 -0300

    feat: add url shortener section and shorten route

---
+++ components/ui/SingleInputForm.tsx
@@ -0,0 +1,211 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
+import type { ComponentChildren } from "preact";
+
+// export interface Props {
+//   header: Header;
+//   links: Links;
+//   background: Background;
+//   footer?: Footer;
+// }
+
+export interface Props {
+  header: Header;
+  background: Background;
+}
+
+export interface Header {
+  /** @description 150px x 150px image recommended */
+  logo?: Logo;
+  /** @format textarea */
+  title?: string;
+  /** @format textarea */
+  description?: string;
+  /**
+   * @format color
+   * @description color to be used in title and description
+   */
+  textColor: string;
+}
+
+export interface Logo {
+  img?: ImageWidget;
+  /** @description alternative text */
+  alt?: string;
+  width?: number;
+  height?: number;
+  /** @description external link on logo */
+  link?: string;
+}
+
+// export interface Links {
+//   items?: Link[];
+//   style: Style;
+// }
+
+// export interface Link {
+//   /** @description 20px transparent png recommended */
+//   icon?: AvailableIcons;
+//   label: string;
+//   /** @format textarea */
+//   href: string;
+// }
+
+// export interface Style {
+//   /**
+//    * @format color
+//    * @description color to be used in link's text
+//    */
+//   textColor: string;
+//   gradientColors: Gradient;
+// }
+
+// export interface Gradient {
+//   /** @description multiple colors will create a gradient effect */
+//   neutral: Neutral[];
+// }
+
+// export interface Neutral {
+//   /**  @format color */
+//   color: string;
+// }
+
+export interface Background {
+  /** @description an image will override any background color */
+  image?: ImageWidget;
+  /** @format color */
+  backgroundColor?: string;
+}
+
+// export interface Footer {
+//   url?: string;
+//   image?: ImageWidget;
+//   /** @description alternative text */
+//   alt?: string;
+//   width?: number;
+//   height?: number;
+//   text?: string;
+// }
+
+function Links(props: Props) {
+  // const { header, background, links } = props;
+  const { header, background } = props;
+  const logo = (
+    <Image
+      decoding="async"
+      src={header.logo?.img || ""}
+      alt={header.logo?.alt}
+      width={header.logo?.width || 171}
+      height={header.logo?.height || 60}
+    />
+  );
+
+  const maybeLink = header?.logo?.link
+    ? <a href={header?.logo?.link!} target="_blank">{logo}</a>
+    : logo;
+
+  // const ColorsNeutralAndHover = {
+  //   color: links.style?.textColor,
+  //   backgroundImage: `linear-gradient(to right, ${
+  //     links.style?.gradientColors.neutral.map((color) => color.color).join(
+  //       ", ",
+  //     )
+  //   })`,
+  // };
+
+  return (
+    <BaseContainer background={background}>
+      <header class="flex flex-col justify-center items-center gap-4">
+        {header?.logo?.img && (
+          <div class="rounded-full p-4">
+            {maybeLink}
+          </div>
+        )}
+
+        {header?.title && (
+          <h1
+            class="text-5xl font-bold text-center"
+            style={{ color: header.textColor }}
+          >
+            {header?.title}
+          </h1>
+        )}
+      
+        {header?.description && (
+          <p
+            style={{ color: header.textColor }}
+          >
+            {header?.description}
+          </p>
+        )}
+
+      </header>
+
+      <main class="w-full">
+        <form method="post" action="/shorten" class="flex gap-2">
+          <div class="w-full">
+            <p>URL</p>
+            <input name="url" class="w-full border-2" />
+          </div>
+          <button type="submit" class="rounded-md bg-lime-600 p-2">Encurtar</button>
+        </form>
+      </main>
+
+      {/* <footer class="flex flex-1 flex-col">
+        {props.footer && (props.footer.image || props.footer.text) && (
+          <div class="mt-auto">
+            <a
+              href={props.footer.url}
+              class="text-xs flex flex-row items-center justify-center gap-1"
+              target="_blank"
+            >
+              {props.footer.text && (
+                <p
+                  style={{ color: header.textColor }}
+                >
+                  {props.footer.text}
+                </p>
+              )}
+              {props.footer.image && (
+                <Image
+                  src={props.footer.image || ""}
+                  alt={props.footer.alt}
+                  width={props.footer.width || 50}
+                  height={props.footer.height || 20}
+                />
+              )}
+            </a>
+          </div>
+        )}
+      </footer> */}
+    </BaseContainer>
+  );
+}
+
+function BaseContainer(props: {
+  children?: ComponentChildren;
+  background?: Props["background"];
+}) {
+  const { image } = props?.background ?? {};
+  const baseClasses = "flex justify-center w-full min-h-screen";
+  const inlineStyle = image ? { background: `url(${image})` } : undefined;
+  const backgroundColors = props?.background?.backgroundColor;
+  const containerClasses = `${baseClasses}`;
+
+  return (
+    <div
+      class={containerClasses}
+      style={{
+        ...inlineStyle,
+        backgroundColor: backgroundColors ? backgroundColors : undefined,
+      }}
+    >
+      <div class="flex flex-col items-center gap-12 p-10 max-w-[640px] w-full">
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default Links;
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,124 +2,131 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$0 from "./loaders/Layouts/ProductCard.tsx";
-import * as $$$1 from "./loaders/List/Sections.tsx";
-import * as $$$$$$0 from "./sections/Gallery.tsx";
-import * as $$$$$$1 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$2 from "./sections/Category/CategoryBanner.tsx";
-import * as $$$$$$3 from "./sections/Category/CategoryList.tsx";
-import * as $$$$$$4 from "./sections/Category/CategoryGrid.tsx";
-import * as $$$$$$5 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$6 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$7 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$8 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$9 from "./sections/Layout/Container.tsx";
-import * as $$$$$$10 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$11 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$12 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$13 from "./sections/Cards/ImageCardsCarousel.tsx";
-import * as $$$$$$14 from "./sections/Cards/IconCardsCarouselWithImage.tsx";
-import * as $$$$$$15 from "./sections/Cards/ImageCardsCarouselWithImage.tsx";
-import * as $$$$$$16 from "./sections/Cards/IconCardsCarousel.tsx";
-import * as $$$$$$17 from "./sections/Animation/Animation.tsx";
-import * as $$$$$$18 from "./sections/Content/Hero.tsx";
-import * as $$$$$$19 from "./sections/Content/Features.tsx";
-import * as $$$$$$20 from "./sections/Content/Intro.tsx";
-import * as $$$$$$21 from "./sections/Content/BlogPosts.tsx";
-import * as $$$$$$22 from "./sections/Content/TextWithImage.tsx";
-import * as $$$$$$23 from "./sections/Content/RichText.tsx";
-import * as $$$$$$24 from "./sections/Content/Partners.tsx";
-import * as $$$$$$25 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$26 from "./sections/Content/Logos.tsx";
-import * as $$$$$$27 from "./sections/Content/CallToAction.tsx";
-import * as $$$$$$28 from "./sections/Content/Table.tsx";
-import * as $$$$$$29 from "./sections/Content/Faq.tsx";
-import * as $$$$$$30 from "./sections/Content/Benefits.tsx";
-import * as $$$$$$31 from "./sections/Content/ImageSection.tsx";
-import * as $$$$$$32 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$33 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$34 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$35 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$36 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$37 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$38 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$39 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$40 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$41 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$42 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$43 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$44 from "./sections/Miscellaneous/Slide.tsx";
-import * as $$$$$$45 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$46 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$47 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$48 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$49 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$50 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$51 from "./sections/Header/Header.tsx";
+import * as $$$0 from "./loaders/List/Sections.tsx";
+import * as $$$1 from "./loaders/Layouts/ProductCard.tsx";
+import * as $$$$$$0 from "./sections/Cards/IconCardsCarousel.tsx";
+import * as $$$$$$1 from "./sections/Cards/ImageCardsCarouselWithImage.tsx";
+import * as $$$$$$2 from "./sections/Cards/ImageCardsCarousel.tsx";
+import * as $$$$$$3 from "./sections/Cards/IconCardsCarouselWithImage.tsx";
+import * as $$$$$$4 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$5 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$6 from "./sections/Miscellaneous/SingleInputForm.tsx";
+import * as $$$$$$7 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$8 from "./sections/Miscellaneous/Slide.tsx";
+import * as $$$$$$9 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$10 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$11 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$12 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$13 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$14 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$15 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$16 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$17 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$18 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$19 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$20 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$21 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$22 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$23 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$24 from "./sections/Layout/Container.tsx";
+import * as $$$$$$25 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$26 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$27 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$28 from "./sections/Animation/Animation.tsx";
+import * as $$$$$$29 from "./sections/Gallery.tsx";
+import * as $$$$$$30 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$31 from "./sections/Content/Features.tsx";
+import * as $$$$$$32 from "./sections/Content/ImageSection.tsx";
+import * as $$$$$$33 from "./sections/Content/Faq.tsx";
+import * as $$$$$$34 from "./sections/Content/TextWithImage.tsx";
+import * as $$$$$$35 from "./sections/Content/Intro.tsx";
+import * as $$$$$$36 from "./sections/Content/Partners.tsx";
+import * as $$$$$$37 from "./sections/Content/Benefits.tsx";
+import * as $$$$$$38 from "./sections/Content/Table.tsx";
+import * as $$$$$$39 from "./sections/Content/BlogPosts.tsx";
+import * as $$$$$$40 from "./sections/Content/Logos.tsx";
+import * as $$$$$$41 from "./sections/Content/RichText.tsx";
+import * as $$$$$$42 from "./sections/Content/Testimonials.tsx";
+import * as $$$$$$43 from "./sections/Content/CallToAction.tsx";
+import * as $$$$$$44 from "./sections/Content/Hero.tsx";
+import * as $$$$$$45 from "./sections/Header/Header.tsx";
+import * as $$$$$$46 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$47 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$48 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$49 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$50 from "./sections/Category/CategoryGrid.tsx";
+import * as $$$$$$51 from "./sections/Category/CategoryList.tsx";
+import * as $$$$$$52 from "./sections/Category/CategoryBanner.tsx";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
 const manifest = {
   "loaders": {
-    "deco-sites/url-shortener/loaders/Layouts/ProductCard.tsx": $$$0,
-    "deco-sites/url-shortener/loaders/List/Sections.tsx": $$$1,
+    "deco-sites/url-shortener/loaders/Layouts/ProductCard.tsx": $$$1,
+    "deco-sites/url-shortener/loaders/List/Sections.tsx": $$$0,
   },
   "sections": {
-    "deco-sites/url-shortener/sections/Animation/Animation.tsx": $$$$$$17,
-    "deco-sites/url-shortener/sections/Cards/IconCardsCarousel.tsx": $$$$$$16,
+    "deco-sites/url-shortener/sections/Animation/Animation.tsx": $$$$$$28,
+    "deco-sites/url-shortener/sections/Cards/IconCardsCarousel.tsx": $$$$$$0,
     "deco-sites/url-shortener/sections/Cards/IconCardsCarouselWithImage.tsx":
-      $$$$$$14,
-    "deco-sites/url-shortener/sections/Cards/ImageCardsCarousel.tsx": $$$$$$13,
+      $$$$$$3,
+    "deco-sites/url-shortener/sections/Cards/ImageCardsCarousel.tsx": $$$$$$2,
     "deco-sites/url-shortener/sections/Cards/ImageCardsCarouselWithImage.tsx":
-      $$$$$$15,
-    "deco-sites/url-shortener/sections/Category/CategoryBanner.tsx": $$$$$$2,
-    "deco-sites/url-shortener/sections/Category/CategoryGrid.tsx": $$$$$$4,
-    "deco-sites/url-shortener/sections/Category/CategoryList.tsx": $$$$$$3,
-    "deco-sites/url-shortener/sections/Content/Benefits.tsx": $$$$$$30,
-    "deco-sites/url-shortener/sections/Content/BlogPosts.tsx": $$$$$$21,
-    "deco-sites/url-shortener/sections/Content/CallToAction.tsx": $$$$$$27,
-    "deco-sites/url-shortener/sections/Content/Faq.tsx": $$$$$$29,
-    "deco-sites/url-shortener/sections/Content/Features.tsx": $$$$$$19,
-    "deco-sites/url-shortener/sections/Content/Hero.tsx": $$$$$$18,
-    "deco-sites/url-shortener/sections/Content/ImageSection.tsx": $$$$$$31,
-    "deco-sites/url-shortener/sections/Content/Intro.tsx": $$$$$$20,
-    "deco-sites/url-shortener/sections/Content/Logos.tsx": $$$$$$26,
-    "deco-sites/url-shortener/sections/Content/Partners.tsx": $$$$$$24,
-    "deco-sites/url-shortener/sections/Content/RichText.tsx": $$$$$$23,
-    "deco-sites/url-shortener/sections/Content/Table.tsx": $$$$$$28,
-    "deco-sites/url-shortener/sections/Content/Testimonials.tsx": $$$$$$25,
-    "deco-sites/url-shortener/sections/Content/TextWithImage.tsx": $$$$$$22,
-    "deco-sites/url-shortener/sections/Footer/Footer.tsx": $$$$$$1,
-    "deco-sites/url-shortener/sections/Gallery.tsx": $$$$$$0,
-    "deco-sites/url-shortener/sections/Header/Header.tsx": $$$$$$51,
-    "deco-sites/url-shortener/sections/Images/BannerGrid.tsx": $$$$$$6,
-    "deco-sites/url-shortener/sections/Images/Carousel.tsx": $$$$$$8,
-    "deco-sites/url-shortener/sections/Images/ImageGallery.tsx": $$$$$$7,
-    "deco-sites/url-shortener/sections/Images/ShoppableBanner.tsx": $$$$$$5,
-    "deco-sites/url-shortener/sections/Layout/Container.tsx": $$$$$$9,
-    "deco-sites/url-shortener/sections/Layout/Flex.tsx": $$$$$$11,
-    "deco-sites/url-shortener/sections/Layout/Grid.tsx": $$$$$$10,
-    "deco-sites/url-shortener/sections/Layout/GridItem.tsx": $$$$$$12,
-    "deco-sites/url-shortener/sections/Links/LinkTree.tsx": $$$$$$48,
-    "deco-sites/url-shortener/sections/Links/Shortcuts.tsx": $$$$$$49,
-    "deco-sites/url-shortener/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$42,
-    "deco-sites/url-shortener/sections/Miscellaneous/CookieConsent.tsx": $$$$$$43,
-    "deco-sites/url-shortener/sections/Miscellaneous/Slide.tsx": $$$$$$44,
-    "deco-sites/url-shortener/sections/Newsletter/Newsletter.tsx": $$$$$$50,
+      $$$$$$1,
+    "deco-sites/url-shortener/sections/Category/CategoryBanner.tsx": $$$$$$52,
+    "deco-sites/url-shortener/sections/Category/CategoryGrid.tsx": $$$$$$50,
+    "deco-sites/url-shortener/sections/Category/CategoryList.tsx": $$$$$$51,
+    "deco-sites/url-shortener/sections/Content/Benefits.tsx": $$$$$$37,
+    "deco-sites/url-shortener/sections/Content/BlogPosts.tsx": $$$$$$39,
+    "deco-sites/url-shortener/sections/Content/CallToAction.tsx": $$$$$$43,
+    "deco-sites/url-shortener/sections/Content/Faq.tsx": $$$$$$33,
+    "deco-sites/url-shortener/sections/Content/Features.tsx": $$$$$$31,
+    "deco-sites/url-shortener/sections/Content/Hero.tsx": $$$$$$44,
+    "deco-sites/url-shortener/sections/Content/ImageSection.tsx": $$$$$$32,
+    "deco-sites/url-shortener/sections/Content/Intro.tsx": $$$$$$35,
+    "deco-sites/url-shortener/sections/Content/Logos.tsx": $$$$$$40,
+    "deco-sites/url-shortener/sections/Content/Partners.tsx": $$$$$$36,
+    "deco-sites/url-shortener/sections/Content/RichText.tsx": $$$$$$41,
+    "deco-sites/url-shortener/sections/Content/Table.tsx": $$$$$$38,
+    "deco-sites/url-shortener/sections/Content/Testimonials.tsx": $$$$$$42,
+    "deco-sites/url-shortener/sections/Content/TextWithImage.tsx": $$$$$$34,
+    "deco-sites/url-shortener/sections/Footer/Footer.tsx": $$$$$$4,
+    "deco-sites/url-shortener/sections/Gallery.tsx": $$$$$$29,
+    "deco-sites/url-shortener/sections/Header/Header.tsx": $$$$$$45,
+    "deco-sites/url-shortener/sections/Images/BannerGrid.tsx": $$$$$$49,
+    "deco-sites/url-shortener/sections/Images/Carousel.tsx": $$$$$$47,
+    "deco-sites/url-shortener/sections/Images/ImageGallery.tsx": $$$$$$48,
+    "deco-sites/url-shortener/sections/Images/ShoppableBanner.tsx": $$$$$$46,
+    "deco-sites/url-shortener/sections/Layout/Container.tsx": $$$$$$24,
+    "deco-sites/url-shortener/sections/Layout/Flex.tsx": $$$$$$26,
+    "deco-sites/url-shortener/sections/Layout/Grid.tsx": $$$$$$27,
+    "deco-sites/url-shortener/sections/Layout/GridItem.tsx": $$$$$$25,
+    "deco-sites/url-shortener/sections/Links/LinkTree.tsx": $$$$$$19,
+    "deco-sites/url-shortener/sections/Links/Shortcuts.tsx": $$$$$$20,
+    "deco-sites/url-shortener/sections/Miscellaneous/CampaignTimer.tsx":
+      $$$$$$7,
+    "deco-sites/url-shortener/sections/Miscellaneous/CookieConsent.tsx":
+      $$$$$$5,
+    "deco-sites/url-shortener/sections/Miscellaneous/SingleInputForm.tsx":
+      $$$$$$6,
+    "deco-sites/url-shortener/sections/Miscellaneous/Slide.tsx": $$$$$$8,
+    "deco-sites/url-shortener/sections/Newsletter/Newsletter.tsx": $$$$$$21,
     "deco-sites/url-shortener/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$38,
-    "deco-sites/url-shortener/sections/Product/ImageGallerySlider.tsx": $$$$$$37,
-    "deco-sites/url-shortener/sections/Product/NotFound.tsx": $$$$$$41,
-    "deco-sites/url-shortener/sections/Product/NotFoundChallenge.tsx": $$$$$$33,
-    "deco-sites/url-shortener/sections/Product/ProductInfo.tsx": $$$$$$35,
-    "deco-sites/url-shortener/sections/Product/ProductShelf.tsx": $$$$$$36,
-    "deco-sites/url-shortener/sections/Product/ProductShelfTabbed.tsx": $$$$$$40,
-    "deco-sites/url-shortener/sections/Product/SearchResult.tsx": $$$$$$34,
-    "deco-sites/url-shortener/sections/Product/ShelfWithImage.tsx": $$$$$$39,
-    "deco-sites/url-shortener/sections/Product/Wishlist.tsx": $$$$$$32,
-    "deco-sites/url-shortener/sections/Social/InstagramPosts.tsx": $$$$$$46,
-    "deco-sites/url-shortener/sections/Social/WhatsApp.tsx": $$$$$$45,
-    "deco-sites/url-shortener/sections/Theme/Theme.tsx": $$$$$$47,
+      $$$$$$18,
+    "deco-sites/url-shortener/sections/Product/ImageGallerySlider.tsx":
+      $$$$$$14,
+    "deco-sites/url-shortener/sections/Product/NotFound.tsx": $$$$$$11,
+    "deco-sites/url-shortener/sections/Product/NotFoundChallenge.tsx": $$$$$$10,
+    "deco-sites/url-shortener/sections/Product/ProductInfo.tsx": $$$$$$12,
+    "deco-sites/url-shortener/sections/Product/ProductShelf.tsx": $$$$$$9,
+    "deco-sites/url-shortener/sections/Product/ProductShelfTabbed.tsx":
+      $$$$$$16,
+    "deco-sites/url-shortener/sections/Product/SearchResult.tsx": $$$$$$15,
+    "deco-sites/url-shortener/sections/Product/ShelfWithImage.tsx": $$$$$$13,
+    "deco-sites/url-shortener/sections/Product/Wishlist.tsx": $$$$$$17,
+    "deco-sites/url-shortener/sections/Social/InstagramPosts.tsx": $$$$$$22,
+    "deco-sites/url-shortener/sections/Social/WhatsApp.tsx": $$$$$$23,
+    "deco-sites/url-shortener/sections/Theme/Theme.tsx": $$$$$$30,
   },
   "apps": {
     "deco-sites/url-shortener/apps/decohub.ts": $$$$$$$$$$$0,
